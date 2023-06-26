d2bbb2d631dfb84643c34b3ca135386ef3723d71
oliarosiak <88856151+oliarosiak@users.noreply.github.com>
2023-06-26 21:09:58 +0000

    Deploying to gh-pages from @ oliarosiak/goit-react-hw-05-movies@01e45fd664c5b82a810a907cdd562a7e8ae8151c 🚀

---
+++ static/js/113.2af90144.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[113],{3113:function(e,t,n){n.r(t);var r=n(184),o=n(1087).Link;t.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Sorry, something went wrong! \ud83d\ude14"}),(0,r.jsx)("p",{children:"Such a page does not exist!"}),(0,r.jsx)(o,{to:"/",children:"Go to the main page"})]})}}}]);
+//# sourceMappingURL=113.2af90144.chunk.js.map
